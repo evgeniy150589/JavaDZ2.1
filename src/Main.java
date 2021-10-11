358a04458e7181185b1d5bd16e6b767da881819d
evgeniy150589 <epritula89@mail.ru>
2021-10-11 12:23:48 +0300

    JavaDZ2.1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 5056;
+                int pricePerMilli = 20;
+                int sumMilli = ticketPrice/pricePerMilli;
+        System.out.println(" Бесплатные Мили "+ sumMilli);
+    }
+}

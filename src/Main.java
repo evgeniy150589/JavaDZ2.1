f554e20445f7b82be855d24f1d037370a878782f
evgeniy150589 <epritula89@mail.ru>
2021-10-23 19:27:54 +0300

    JavaDZ2.1_correction_v1

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         int ticketPrice = 5056;
-                int pricePerMilli = 20;
-                int sumMilli = ticketPrice/pricePerMilli;
-        System.out.println(" Бесплатные Мили "+ sumMilli);
+        int pricePerMilli = 20;
+        int sumMilli = ticketPrice / pricePerMilli;
+        System.out.println(" Бесплатные Мили " + sumMilli);
     }
 }
